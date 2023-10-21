ae888ce29608307146ba72240bf34ba92fdb5d91
Vadim Medvedev <vadim130809@gmail.com>
2023-10-21 08:32:09 +0300

    Create chat lists panel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,22 @@ export default function Header({auth}) {
     return (
         <AppBar position='sticky'>
             <Toolbar>
-                <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                    Коллаж
+                <Typography variant="h4" sx={{ flexGrow: 1 }}>
+                    <Link to='/'>Коллаж</Link>
                 </Typography>
 
+                <nav style={{flexGrow: 1.2}}>
+                    <ul>
+                        <li>
+                            <Link to="/chats" style={{fontSize: '2rem'}}>Чаты</Link>
+                        </li>
+                    </ul>
+                </nav>
+
                 {auth 
-                    ? <Button variant="contained">
-                        <Link to="/account" style={{all: "unset"}}>Ваш аккаунт</Link>
-                    </Button>
+                    ? <Link to="/account" style={{all: "unset"}}>
+                            <Button variant="contained">Ваш аккаунт</Button>
+                    </Link>
                    : <form name="registerForm">
                         <Button variant="contained">
                             <Link to="/log" style={{all: "unset"}}>Войти</Link>

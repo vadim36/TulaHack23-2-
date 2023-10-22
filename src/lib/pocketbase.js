c3647d076984f8e64a879d9a71929cf45a9175e1
Vadim Medvedev <vadim130809@gmail.com>
2023-10-22 12:45:45 +0300

    add database

---
+++ src/lib/pocketbase.js
@@ -0,0 +1,8 @@
+import PocketBase from 'pocketbase';
+
+export const pb = new PocketBase('http://127.0.0.1:8090');
+export const currentUser = writable(pb.authStore.model);
+
+pb.authStore.onChange((auth) => {
+    currentUser.set(pb.authStore.model);
+});
